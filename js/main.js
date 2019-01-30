05d17b91cce6d309aac03527d008fc9b44f26c33
tanya-jayant <tanyajayant27@gmail.com>
2019-01-30 11:30:34 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,2 @@
+(function($){"use strict"
+$(window).on('load',function(){$("#preloader").delay(600).fadeOut();});$('.navbar-toggle').on('click',function(){$('#header').toggleClass('nav-collapse')});})(jQuery);
